1193b18addf7e0bb968129e1f0d2bff2646d5221
LCN29 <CanLee529@gmail.com>
2020-02-16 09:56:54 +0800

    init the project structure

---
+++ mall-web/src/main/java/com/lcn29/mall/MallApplicationLauncher.java
@@ -0,0 +1,16 @@
+package com.lcn29.mall;
+
+/**
+ * <pre>
+ * Application Launcher
+ * </pre>
+ *
+ * @author LCN
+ * @date 2020-02-16 09:54
+ */
+public class MallApplicationLauncher {
+
+    public static void main(String[] args) {
+
+    }
+}
